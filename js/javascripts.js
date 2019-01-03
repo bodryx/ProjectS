f7eb6462b066e06d2cb9ba929dc022bfa2c51785
BRS <bodryx@gmail.com>
2019-01-03 09:45:01 -0500

    #8 day or night

--- js/javascripts.js
+++ js/javascripts.js
@@ -1,7 +1,21 @@
+function getYear() {
+  	var d = new Date();
+   	var n = d.getFullYear();
+   	return n;
+}
 
-        function getYear() {
-        	var d = new Date();
-        	var n = d.getFullYear();
-        	return n;
-        }
-        document.getElementById("year").innerHTML = getYear();
+document.getElementById("year").innerHTML = getYear();
+
+function dayOrNight() {
+    var timeFlag = "night";
+    var d = new Date();
+    var hours = d.getHours();
+    if ((hours > 7) && (hours < 18)){
+	  	timeFlag = "day";
+    }
+    return timeFlag;
+}
+
+var x = document.getElementsByClassName("hero_area");
+x[0].style = "background-image: url(img/" + dayOrNight() + ".jpg";	
+
